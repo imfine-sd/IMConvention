bb58a9ab20510067938ae44559ce02cb7aecf166
skim <suyoung.kim@im-fine.co.kr>
2020-11-02 18:31:40 +0900

    MM

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -8,9 +8,22 @@ importScripts('https://www.gstatic.com/firebasejs/7.8.0/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-    'messagingSenderId': '371247898053'
+    apiKey: "AIzaSyBtnFzSNMp30pWM2CNva-U9ZM1sdE8lzts",
+    authDomain: "fir-ca8ad.firebaseapp.com",
+    databaseURL: "https://fir-ca8ad.firebaseio.com",
+    projectId: "fir-ca8ad",
+    storageBucket: "fir-ca8ad.appspot.com",
+    messagingSenderId: '371247898053',
+    appId: "1:371247898053:web:a657c9fb4898a89947bfac",
+    measurementId: "G-BWCDY22QJR"
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 // firebase.messaging();
+const messaging = firebase.messaging();
+messaging.onMessage((payload) => {
+    console.log('Message received. ', payload);
+    // ...
+  });
+console.log("MM", messaging)
--- precache-manifest.7c417abbbce88e2f7a2538fa5a099618.js
+++ precache-manifest.7c417abbbce88e2f7a2538fa5a099618.js
@@ -1,14 +1,14 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "510d331addd7f302f28b",
+    "revision": "72b796990ddd30918e3c",
     "url": "/IMConvention/css/app.ba422f61.css"
   },
   {
-    "revision": "da3cb873ca04ff378f70",
+    "revision": "d7d10954a98a26289583",
     "url": "/IMConvention/css/chunk-vendors.b44cf432.css"
   },
   {
-    "revision": "cea027b9756aa013d2b0f4f9efca2d80",
+    "revision": "2f72ec0f82ff1a53647261e5e00fa246",
     "url": "/IMConvention/firebase-messaging-sw.js"
   },
   {
@@ -16,16 +16,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/IMConvention/img/LOGO-1.a61f62f6.gif"
   },
   {
-    "revision": "3806ac411b3090df322ead1dec95ea6a",
+    "revision": "efe34b85f29b28a58f8bea869007a6fc",
     "url": "/IMConvention/index.html"
   },
   {
-    "revision": "510d331addd7f302f28b",
-    "url": "/IMConvention/js/app.2050b548.js"
+    "revision": "72b796990ddd30918e3c",
+    "url": "/IMConvention/js/app.7b15e1ac.js"
   },
   {
-    "revision": "da3cb873ca04ff378f70",
-    "url": "/IMConvention/js/chunk-vendors.712d0e86.js"
+    "revision": "d7d10954a98a26289583",
+    "url": "/IMConvention/js/chunk-vendors.ee75e278.js"
   },
   {
     "revision": "662eb7db2c9dbd93bee77a79f70c171a",
